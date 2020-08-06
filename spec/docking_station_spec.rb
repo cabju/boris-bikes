ed961e52f313c933881fff9f1e6239438be98009
Carlo <Carlo_junior@hotmail.co.uk>
2020-08-06 10:22:39 +0100

    docking_station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,9 @@ describe DockingStation do
 it 'expecting docking station to respond to method "release bike"' do
   expect(DockingStation.new).to respond_to(:release_bike)
   end
+it 'expects "true" if the bike is working' do
+  expect(DockingStation.new.release_bike.working?).to eq true
+  end
 end
 
 
